8e900b8719f738602c2de756de78beefbe7167bf
pranavlabdhe <plabdhe2000@gmail.com>
2024-02-19 17:48:47 +0530

    Added env

--- api/index.js
+++ api/index.js
@@ -40,7 +40,7 @@ app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 app.use('/api/textToSpeechfun',translateRoute);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
